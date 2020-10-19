b2b8f899248ed4d271f20a59c708e7be2a13e81c
richard slettevoll <richard_slettevoll@hotmail.com>
2020-10-19 14:50:29 +0200

    add fetch

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
 
+async function fetchText() {
+  let response = await fetch("/api/1")
+
+  console.log(response.status) // 200
+  console.log(response.statusText) // OK
+
+  if (response.status === 200) {
+    return await response.text()
+    // handle data
+  }
+}
+
 export default function Home() {
-  return <div>Hello world from gatsby!</div>
+  return (
+    <>
+      <div>Hello world from frontend homepage!</div>
+      <button
+        onClick={() => {
+          console.log(fetchText())
+        }}
+      >
+        Fetch from api
+      </button>
+    </>
+  )
 }
